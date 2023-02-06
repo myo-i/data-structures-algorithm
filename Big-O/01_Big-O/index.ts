5f324cd2d32ee2624d4082bb7fb864e8330096c6
myo-i <Imiyo0521@gmail.com>
2023-02-07 00:58:20 +0900

    ✨ Big-O導入

---
+++ Big-O/01_Big-O/index.ts
@@ -0,0 +1,15 @@
+const cat = ["cat"]
+const catInDog = ["dog","dog","dog","dog","cat","dog","dog","dog","dog","dog",]
+const large = new Array(10000).fill("cat")
+function catchcat(array: string[]) {
+    let t0 = performance.now();
+    for (let i=0;i<array.length; i++) {
+        if(array[i].includes("cat")) {
+            console.log("Catch CAT!");
+        }
+    }
+    let t1 = performance.now();
+    console.log("Call to Catch CAT took "+(t1-t0)+"milliseconds");
+}
+
+catchcat(large)
