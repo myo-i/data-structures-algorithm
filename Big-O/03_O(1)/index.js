87cf535eb966b4920f7abdb5823f68c185473142
myo-i <Imiyo0521@gmail.com>
2023-02-11 02:40:34 +0900

    コンパイル時の変更

--- Big-O/03_O(1)/index.js
+++ Big-O/03_O(1)/index.js
@@ -1,7 +1,7 @@
 "use strict";
 const boxes = [0, 1, 2, 3, 4, 5];
 function logTwoItems(boxes) {
-    console.log(boxes[0]);
-    console.log(boxes[1]);
+    console.log(boxes[0]); // O(1)
+    console.log(boxes[1]); // O(1)
 }
-logTwoItems(boxes);
+logTwoItems(boxes); // O(2)
