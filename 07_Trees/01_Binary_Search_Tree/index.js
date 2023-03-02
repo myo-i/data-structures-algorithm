2d4a9e80aa8cd39d899ca4c3c80c0c7bb6baa229
myo-i <Imiyo0521@gmail.com>
2023-03-02 10:19:18 +0900

    ✨ remove実装（自作）たぶんできてると思う

--- 07_Trees/01_Binary_Search_Tree/index.js
+++ 07_Trees/01_Binary_Search_Tree/index.js
@@ -45,12 +45,62 @@ class BinarySearchTree {
         }
         let currentNode = this.root;
         while(currentNode) {
-            if(value < currentNode.value) currentNode = currentNode.left;
-            else if(value > currentNode.value) currentNode = currentNode.right;
-            else if(value === currentNode.value) return true;
+            if(value < currentNode.value) 
+                currentNode = currentNode.left;
+            else if(value > currentNode.value) 
+                currentNode = currentNode.right;
+            else if(value === currentNode.value) 
+                return true;
         }
         return false;
     }
+
+    // 削除対象の次に大きい数字が代わりに入っている
+    remove(value) {
+        let currentNode = this.root;
+        // 対象を探し、対象のノードがあれば取得
+        while(value !== currentNode.value) {
+            if(value < currentNode.value) 
+                currentNode = currentNode.left;
+            else if(value > currentNode.value) 
+                currentNode = currentNode.right;
+            else if(!currentNode.left && !currentNode.right) {
+                return "There is no target";
+            }
+        }
+        // currentNodeには削除対象が入っている
+        // currentNodeの次の要素がない場合はcurrentNode=削除対象
+        if(!currentNode.right && !currentNode.left) {
+            // 削除処理
+            currentNode.value = null;
+            return this;
+        // currentNodeのleftがない場合はrightをそのままくっつける
+        } else if(!currentNode.left) {
+            currentNode.value = currentNode.right.value;
+            currentNode.right = currentNode.right.value.right;
+            return this;
+        // currentNodeのrightがない場合はleftをそのままくっつける
+        } else if(!currentNode.right) {
+            currentNode.value = currentNode.left.value;
+            currentNode.left = currentNode.left.value.left;
+        // currentNodeにleft, rightどちらも存在する場合、valueの次に大きい値を削除対象の値と入れ替える
+        // visualgoで試した感じだとそんなロジックっぽいから
+        } else {
+            let nextLargest = currentNode;
+            // 削除対象の次に大きい値を探す
+            // currentNode = currentNode.right;
+            if(!nextLargest.right.left) {
+                currentNode.value = nextLargest.right.value;
+                currentNode.right = nextLargest.right.value.right;
+                return this;
+            }
+            while(nextLargest.left) {
+                nextLargest = nextLargest.left
+            }
+            currentNode.value = nextLargest.value;
+        }
+    }
+
     print() {
         console.log(this);
     }
@@ -70,7 +120,14 @@ console.log(myTree.lookup(170));
 console.log(myTree.lookup(15));
 console.log(myTree.lookup(17));
 console.log(myTree.lookup(9));
+myTree.remove(21)
+myTree.print();
+
 // console.log(traverse(myTree));
+
+//      9
+//  3       21
+//1   7  15    170
 
 function traverse(node) {
     const tree = {value: node.value};
